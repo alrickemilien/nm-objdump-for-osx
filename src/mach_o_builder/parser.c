b88fa1dead7c58b35122c4e144c57e1ca7a4d768
Alrick EMILIEN <aemilien@e1r9p8.42.fr>
2018-11-07 19:05:02 +0100

    ++ struct style

--- src/mach_o_builder/parser.c
+++ src/mach_o_builder/parser.c
@@ -1,5 +1,117 @@
 #include "mach_o_builder.h"
 
+
+struct mapping {
+  char *key;
+  void *value;
+};
+
+static t_mach_o_builder builder;
+
+  static const char *state_keys_g[] = {
+    "[HEADER]",
+    "[LOAD_COMMAND]",
+    "[SECTION_COMMAND]",
+    NULL,
+  };
+
+  /*
+  ** struct mach_header {
+  **         uint32_t        magic;          // mach magic number identifier
+  **         cpu_type_t      cputype;        // cpu specifier
+  **         cpu_subtype_t   cpusubtype;     // machine specifier
+  **         uint32_t        filetype;       // type of file
+  **         uint32_t        ncmds;          // number of load commands
+  **         uint32_t        sizeofcmds;     // the size of all the load commands
+  **         uint32_t        flags;          // flags
+  ** };
+  **
+  ** struct mach_header_64 {
+  **         uint32_t        magic;          // mach magic number identifier
+  **         cpu_type_t      cputype;        // cpu specifier
+  **         cpu_subtype_t   cpusubtype;     // machine specifier
+  **         uint32_t        filetype;       // type of file
+  **         uint32_t        ncmds;          // number of load commands
+  **         uint32_t        sizeofcmds;     // the size of all the load commands
+  **         uint32_t        flags;          // flags
+  **         uint32_t        reserved;       // reserved
+  ** };
+  */
+
+  static const struct mapping mach_header_keys_map_g[] = {
+    { "magic_number", &builder.header.header.magic },
+    { "cputype", &builder.header.header.cputype },
+    { "cpusubtype", &builder.header.header.cpusubtype },
+    { "filetype", &builder.header.header.filetype },
+    { "ncmds", &builder.header.header.ncmds },
+    { "sizeofcmds", &builder.header.header.sizeofcmds },
+    { "flags", &builder.header.header_64.flags },
+    { "reserved", &builder.header.header_64.reserved },
+    { NULL, NULL},
+  };
+
+  /*
+  ** struct load_command {
+  **        uint32_t cmd;           // type of load command
+  **        uint32_t cmdsize;       // total size of command in bytes
+  ** };
+  */
+
+  static const struct mapping load_command_g[] = {
+    { "cmd", &builder.cmd->lc.cmd },
+    { "cmdsize", &builder.cmd->lc.cmdsize },
+    { "section_architecture", &builder.cmd->lc.section_architecture },
+    { NULL, NULL},
+  };
+
+  /*
+  ** struct section { // for 32-bit architectures
+  **         char            sectname[16];   // name of this section
+  **         char            segname[16];    // segment this section goes in
+  **         uint32_t        addr;           // memory address of this section
+  **         uint32_t        size;           // size in bytes of this section
+  **         uint32_t        offset;         // file offset of this section
+  **         uint32_t        align;          // section alignment (power of 2)
+  **         uint32_t        reloff;         // file offset of relocation entries
+  **         uint32_t        nreloc;         // number of relocation entries
+  **         uint32_t        flags;          // flags (section type and attributes
+  **         uint32_t        reserved1;      // reserved (for offset or index)
+  **         uint32_t        reserved2;      // reserved (for count or sizeof)
+  ** };
+  **
+  ** struct section_64 { // for 64-bit architectures
+  **         char            sectname[16];   // name of this section
+  **         char            segname[16];    // segment this section goes in
+  **         uint64_t        addr;           // memory address of this section
+  **         uint64_t        size;           // size in bytes of this section
+  **         uint32_t        offset;         // file offset of this section
+  **         uint32_t        align;          // section alignment (power of 2)
+  **         uint32_t        reloff;         // file offset of relocation entries
+  **         uint32_t        nreloc;         // number of relocation entries
+  **         uint32_t        flags;          // flags (section type and attributes
+  **         uint32_t        reserved1;      // reserved (for offset or index)
+  **         uint32_t        reserved2;      // reserved (for count or sizeof)
+  **         uint32_t        reserved3;      // reserved
+  ** };
+  */
+
+  static const struct mapping section_keys_map_g[] = {
+    {"section_type", &builder.cmd->section.section_type },
+    {"sectname[16]", &builder.cmd->section.sectname },
+    {"segname[16]", &builder.cmd->section.segname },
+    {"addr", &builder.cmd->section.addr },
+    {"size", &builder.cmd->section.size },
+    {"offset", &builder.cmd->section.offset },
+    {"align", &builder.cmd->section.align },
+    {"reloff", &builder.cmd->section.reloff },
+    {"nreloc", &builder.cmd->section.nreloc },
+    {"flags", &builder.cmd->section.flags },
+    {"reserved1", &builder.cmd->section.reserved1 },
+    {"reserved2", &builder.cmd->section.reserved2 },
+    {"reserved3", &builder.cmd->section_64.reserved2 },
+    { NULL, NULL},
+  };
+
 static const int HEADER_STATE = 0;
 //static const int LOAD_COMMAND_STATE = 1;
 
@@ -19,7 +131,7 @@ static int find_valid_key(const char *str, int state)
 
   (void)state;
 
-  const char **map[] = {
+  const struct mapping *map[] = {
     mach_header_keys_map_g,
     load_command_g,
     section_keys_map_g,
@@ -31,15 +143,15 @@ static int find_valid_key(const char *str, int state)
   while(map[i])
   {
     j = 0;
-    while (map[i][j])
+    while (map[i][j].key)
     {
-      if (!ft_strcmp(map[i][j], str))
+      if (!ft_strcmp(map[i][j].key, str))
       {
         // If the state does not accept this key
         if ((int)i != state)
             ret = 0;
         else
-          return (1);
+          return (j);
 
       }
 
@@ -116,15 +228,13 @@ static bool is_state_indication(const char *str, int *state)
   return (false);
 }
 
-static apply_property(t_mach_o_builder *builder, int key, const char *value_str)
+static void apply_property(int state, int key, const char *value_str)
 {
-
+  (void)state;
+  (void)key;
+  (void)value_str;
 }
 
-struct by_chelou {
-  char *key;
-  void *value;
-};
 
 /*
 ** The state variable tells where are in the parsing routine
@@ -132,117 +242,13 @@ struct by_chelou {
 ** Are we waiting a load command ?
 */
 
-int build_mach_o_from_conf(t_mach_o_builder *builder, const char *path)
+int build_mach_o_from_conf(t_mach_o_builder *b, const char *path)
 {
 	int		fd;
 	char	*line;
   char  **property;
   int    state;
   int    ret;
-
-  static const char *state_keys_g[] = {
-    "[HEADER]",
-    "[LOAD_COMMAND]",
-    "[SECTION_COMMAND]",
-    NULL,
-  };
-
-  /*
-  ** struct mach_header {
-  **         uint32_t        magic;          // mach magic number identifier
-  **         cpu_type_t      cputype;        // cpu specifier
-  **         cpu_subtype_t   cpusubtype;     // machine specifier
-  **         uint32_t        filetype;       // type of file
-  **         uint32_t        ncmds;          // number of load commands
-  **         uint32_t        sizeofcmds;     // the size of all the load commands
-  **         uint32_t        flags;          // flags
-  ** };
-  **
-  ** struct mach_header_64 {
-  **         uint32_t        magic;          // mach magic number identifier
-  **         cpu_type_t      cputype;        // cpu specifier
-  **         cpu_subtype_t   cpusubtype;     // machine specifier
-  **         uint32_t        filetype;       // type of file
-  **         uint32_t        ncmds;          // number of load commands
-  **         uint32_t        sizeofcmds;     // the size of all the load commands
-  **         uint32_t        flags;          // flags
-  **         uint32_t        reserved;       // reserved
-  ** };
-  */
-
-  static const struct by_chelou mach_header_keys_map_g[] = {
-    { "magic_number", &builder->header.header.magic },
-    { "cputype", &builder->header.header.cputype },
-    { "cpusubtype", &builder->header.header.cpusubtype },
-    { "filetype", &builder->header.header.filetype },
-    { "ncmds", &builder->header.header.ncmds },
-    { "sizeofcmds", &builder->header.header.sizeofcmds },
-    { "flags", &builder->header.header64.flags },
-    { "reserved", &builder->header.header64.reserved },
-    NULL,
-  };
-
-  /*
-  ** struct load_command {
-  **        uint32_t cmd;           // type of load command
-  **        uint32_t cmdsize;       // total size of command in bytes
-  ** };
-  */
-
-  static const struct by_chelou load_command_g[] = {
-    { "cmd", &builder->cmd->lc.cmd },
-    { "cmdsize", &builder->cmd->lc.cmdsize },
-    { "section_architecture", &builder->cmd->lc.section_architecture },
-    NULL,
-  };
-
-  /*
-  ** struct section { // for 32-bit architectures
-  **         char            sectname[16];   // name of this section
-  **         char            segname[16];    // segment this section goes in
-  **         uint32_t        addr;           // memory address of this section
-  **         uint32_t        size;           // size in bytes of this section
-  **         uint32_t        offset;         // file offset of this section
-  **         uint32_t        align;          // section alignment (power of 2)
-  **         uint32_t        reloff;         // file offset of relocation entries
-  **         uint32_t        nreloc;         // number of relocation entries
-  **         uint32_t        flags;          // flags (section type and attributes
-  **         uint32_t        reserved1;      // reserved (for offset or index)
-  **         uint32_t        reserved2;      // reserved (for count or sizeof)
-  ** };
-  **
-  ** struct section_64 { // for 64-bit architectures
-  **         char            sectname[16];   // name of this section
-  **         char            segname[16];    // segment this section goes in
-  **         uint64_t        addr;           // memory address of this section
-  **         uint64_t        size;           // size in bytes of this section
-  **         uint32_t        offset;         // file offset of this section
-  **         uint32_t        align;          // section alignment (power of 2)
-  **         uint32_t        reloff;         // file offset of relocation entries
-  **         uint32_t        nreloc;         // number of relocation entries
-  **         uint32_t        flags;          // flags (section type and attributes
-  **         uint32_t        reserved1;      // reserved (for offset or index)
-  **         uint32_t        reserved2;      // reserved (for count or sizeof)
-  **         uint32_t        reserved3;      // reserved
-  ** };
-  */
-
-  static const struct by_chelou section_keys_map_g[] = {
-    {"section_type", &builder->cmd->section.section_type },
-    {"sectname[16]", &builder->cmd->section.sectname },
-    {"segname[16]", &builder->cmd->section.segname },
-    {"addr", &builder->cmd->section.addr },
-    {"size", &builder->cmd->section.size },
-    {"offset", &builder->cmd->section.offset },
-    {"align", &builder->cmd->section.align },
-    {"reloff", &builder->cmd->section.reloff },
-    {"nreloc", &builder->cmd->section.nreloc },
-    {"flags", &builder->cmd->section.flags },
-    {"reserved1", &builder->cmd->section.reserved1 },
-    {"reserved2", &builder->cmd->section.reserved2 },
-    {"reserved3", &builder->cmd->section_64.reserved2 },
-    NULL,
-  };
 
 	fd = load_file_descriptor(path);
 
@@ -275,11 +281,13 @@ int build_mach_o_from_conf(t_mach_o_builder *builder, const char *path)
     }
 
     if (ret > 0)
-      apply_property(ret, property[3]);
+      apply_property(state, ret, property[3]);
 
     clear_array(property);
     free(line);
 	}
+
+  memcpy(b, &builder, sizeof(t_mach_o_builder));
 
 	return (0);
 }

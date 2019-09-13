1bf316b125fc3790ace3f4813369a2a1a2eb1b73
Alricko <alricko@Alrickos-MacBook-Pro.local>
2019-09-13 15:11:56 +0200

    Integrate fmt error into options error handling

--- src/nm/error.c
+++
@@ -1,9 +0,0 @@
-#include "nm.h"
-
-void print_error_on_option(const char *msg, const char *option)
-{
-	ft_putstr_fd(msg, 2);
-	ft_putstr_fd("'", 2);
-	ft_putstr_fd(option, 2);
-	ft_putendl_fd("'.", 2);
-}

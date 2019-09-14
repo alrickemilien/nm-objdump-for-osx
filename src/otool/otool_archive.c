fe1b24cfb45c1ade3406e2c106d6d1ba6e7e0c94
Alrick EMILIEN <aemilien@e1r11p23.42.fr>
2019-09-14 15:39:52 +0200

    ++ otool

--- src/otool/otool_archive.c
+++ src/otool/otool_archive.c
@@ -2,7 +2,26 @@
 
 int	otool_archive(t_mach_o *file, t_options *options)
 {
-        (void)file;
-    (void)options;
-    return (0);
+	size_t  i;
+	bool    error;
+
+	i = 0;
+	error = 0;
+	while (42)
+	{
+		if (load_archive_nth_member(file, i, &error) == -1)
+		{
+			if (error)
+				return (0);
+			i++;
+			continue ;
+		}
+		printf("%s(%s):\n", file->path, file->archive_member_header.member_name);
+        if (otool_object(file, options) == -1)
+		{
+			LOGDEBUG("INFO otool_archive ended zith code -1\n");
+			// return (-1);
+		}
+		i++;
+	}
 }
--- src/otool/otool_fat_archive.c
+++ src/otool/otool_fat_archive.c
@@ -1,8 +1,60 @@
 #include "otool.h"
+#include <mach-o/arch.h>
+
+static int	otool_current_arch(t_mach_o *file, t_options *options)
+{
+	if (file->arch_type == OBJECT_FILE)
+		return (otool_object(file, options));
+	else if (file->arch_type == FAT_FILE)
+		return (otool_fat_archive(file, options));
+	else if (file->arch_type == ARCHIVE_FILE)
+	{
+		printf("Archive : %s\n", file->path);
+		return (otool_archive(file, options));
+	}
+	return (-1);
+}
+
+static int	otool_all_fat_archs(t_mach_o *file, t_options *options)
+{
+	size_t			i;
+
+	i = 0;
+	while (i < file->fat_header->nfat_arch)
+	{
+		if (load_fat_archive_nth_arch(file, i) == -1)
+		{
+			i++;
+			continue ;
+		}
+		if (file->fat_header->nfat_arch != 1)
+			printf("%s (architecture %s):\n", file->path,
+			cpu_type_name(file->fat_archs ? file->fat_archs[i].cputype
+			: file->fat_archs_64[i].cputype));
+		else
+			printf("%s:\n", file->path);
+		if (otool_current_arch(file, options) == -1)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
 
 int	otool_fat_archive(t_mach_o *file, t_options *options)
 {
-    (void)file;
-    (void)options;
-    return (0);
+	int32_t				narch_for_arch;
+	const NXArchInfo	*host_arch;
+
+	host_arch = NXGetLocalArchInfo();
+	if ((narch_for_arch = find_fat_archive_architecture(file,
+		host_arch->cputype | CPU_ARCH_ABI64, host_arch->cpusubtype)) == -1)
+		return (otool_all_fat_archs(file, options));
+	else
+	{
+		if (load_fat_archive_nth_arch(file, (uint32_t)narch_for_arch) == -1)
+			return (-1);
+		if (file->arch_type != ARCHIVE_FILE)
+			printf("%s:\n", file->path);
+		return (otool_current_arch(file, options));
+	}
 }
